41324ac208b82066bf73c595c26093824ad55459
Enkhbaatar Idersaikhan <roberthdoll@gmail.com>
2025-10-07 02:30:09 +0100

    Learned writing React Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 import "./App.css";
+import eddie from "./images/eddie.jpg";
 
 function Header({ name, year }) {
   return (
@@ -9,13 +11,48 @@ function Header({ name, year }) {
   );
 }
 
+const items = [
+  "Mac and Cheese",
+  "Salmon with Potatoes",
+  "Right Food",
+  "Tofu with Mash",
+  "Asian Poison",
+];
+
+const dishObjects = items.map((dish, i) => ({
+  id: i,
+  title: dish,
+}));
+
+function Main({ dishes }) {
+  return (
+    <>
+      <div>
+        <h2>Welcome to this beautiful restaurant</h2>
+      </div>
+      <main>
+        <img
+          src="https://github.com/SylerEdd.png"
+          height={200}
+          alt="A photo of a handsome dude"
+        />
+        <ul>
+          {dishes.map((dish) => (
+            <li key={dish.id} style={{ listStyleType: "none" }}>
+              {dish.title}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header name="Alex" year={new Date().getFullYear()} />
-      <main>
-        <h2>We serve the most delicious food around</h2>
-      </main>
+      <Main dishes={dishObjects} />
     </div>
   );
 }
